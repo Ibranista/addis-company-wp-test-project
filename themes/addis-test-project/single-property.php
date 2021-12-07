3f892718b99807737dbfc34e2430d815b236e65f
Ibranista <68871293+Ibranista@users.noreply.github.com>
2021-12-07 09:08:33 -0800

    remove the wp_loop

--- themes/addis-test-project/single-property.php
+++ themes/addis-test-project/single-property.php
@@ -64,20 +64,7 @@ get_header(); ?>
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
-		<?php
-		while (have_posts()) : the_post();
 
-			get_template_part('template-parts/content', get_post_format());
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if (comments_open() || get_comments_number()) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
 	</div><!-- #main -->
 </section><!-- #primary -->
 
